0f661263ff968c1387e5c906086c31d5f401f5b2
SohamAgawane <93815863+SohamAgawane@users.noreply.github.com>
2024-07-23 14:26:10 +0530

    git add

---
+++ 01_basics/18-else-if.js
@@ -0,0 +1,22 @@
+//Else if :
+
+let marks = 70;
+
+if(marks <= 30){
+    console.log("Fail")
+}
+else if(marks <=50){
+    console.log("Pass : Grade = E")
+}
+else if(marks <=65){
+    console.log("Pass : Grade = D")
+}
+else if(marks <=75){
+    console.log("Pass : Grade = C")
+}
+else if(marks <=85){
+    console.log("Pass : Grade = B")
+}
+else{
+    console.log("Pass : Grade = A")
+}
